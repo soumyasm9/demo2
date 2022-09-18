9b7a688e0c37f1c3db3245361da6f39598ce8542
Shyam Mittapally <shyamreddymittapally@Shyams-MBP.attlocal.net>
2022-09-18 00:36:08 -0500

    java

--- src/main/java/com/jenkinsdemo/demo/rest/MainController.java
+++ src/main/java/com/jenkinsdemo/demo/rest/MainController.java
@@ -1,7 +1,5 @@
 package com.jenkinsdemo.demo.rest;
 
-import javax.xml.ws.RespectBinding;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
